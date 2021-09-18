bba372f2319b59a1788f377f352e81e30611bb91
amprusty <amrit2995@gmail.com>
2021-09-18 13:15:47 +0530

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+class blogposts(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(max_length = 1000)
+    date = models.DateField(default=datetime.now())
+
+    def __str__(self):
+        return self.title
